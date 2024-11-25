303aea1bd1959070cda95eeea00e64e67838226e
Andreea <andreea.celmare@gmail.com>
2024-11-25 11:29:44 +0100

    first commit

---
+++ src/main/java/com/example/ClinicaVeterinaria_Asuncion/ClinicaVeterinariaAsuncionApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClinicaVeterinaria_Asuncion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaVeterinariaAsuncionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaVeterinariaAsuncionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ClinicaVeterinaria_Asuncion/ClinicaVeterinariaAsuncionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClinicaVeterinaria_Asuncion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVeterinariaAsuncionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
