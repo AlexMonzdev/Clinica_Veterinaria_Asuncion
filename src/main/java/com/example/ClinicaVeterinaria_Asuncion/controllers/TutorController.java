3025f8fea37f9a5837e7b73784ff0399c6f1b4cf
Andreea <andreea.celmare@gmail.com>
2024-12-04 09:13:03 +0100

    feat: change tutor PostMapping status

--- src/main/java/com/example/ClinicaVeterinaria_Asuncion/controllers/TutorController.java
+++ src/main/java/com/example/ClinicaVeterinaria_Asuncion/controllers/TutorController.java
@@ -23,7 +23,7 @@ public class TutorController {
     @PostMapping ("/tutor")
     public ResponseEntity<Tutor> addTutor(@RequestBody TutorRequest tutorRequest) {
         tutorServices.createTutor(tutorRequest);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping ("/tutor")
